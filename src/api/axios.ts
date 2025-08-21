9ec022e175f08e14275b7f81b67387c483866544
ThakurShivam12 <shivamrudra2006@gmail.com>
2025-08-21 23:45:54 +0530

    change baseURL to coonect with deployed backend

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/auth';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://pms-backend-two.vercel.app/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
